18d589d975e205a270f6d2f5c27e50043f0a0ed4
jaiyalas <jaiyalas@gmail.com>
2015-10-15 00:20:45 +0800

    add an universal module for Haha.Lambda.NamedVar.*

---
+++ src/Haha/Lambda/NamedVar/Lambda.hs
@@ -0,0 +1,16 @@
+module Haha.Lambda.NamedVar.Lambda
+  ( module Haha.Lambda.NamedVar.Term
+  , module Haha.Lambda.NamedVar.NameSugar
+  , module Haha.Lambda.NamedVar.InputSet
+  , module Haha.Lambda.NamedVar.Redex
+  , module Haha.Lambda.NamedVar.Reduction
+  , module Haha.Lambda.NamedVar.Num
+  )
+where
+
+import Haha.Lambda.NamedVar.Term
+import Haha.Lambda.NamedVar.NameSugar
+import Haha.Lambda.NamedVar.InputSet
+import Haha.Lambda.NamedVar.Redex
+import Haha.Lambda.NamedVar.Reduction
+import Haha.Lambda.NamedVar.Num
